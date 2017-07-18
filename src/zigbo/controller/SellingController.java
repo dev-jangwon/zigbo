e89eaae7437475a5e6ea2250d16b9a200f50c6ab
sykimm <95thdus95@gmail.com>
2017-07-18 15:17:21 +0900

    Controller Update

--- src/zigbo/controller/SellingController.java
+++ src/zigbo/controller/SellingController.java
@@ -1,7 +1,6 @@
 package zigbo.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,28 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 import zigbo.model.ZigboService;
 import zigbo.model.dto.MemberDTO;
 
-public class MemberController extends HttpServlet {
+public class SellingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("euc-kr");
 		String command = request.getParameter("command");
-		
-		try{
-			if(command.equals("addMember")){
-				addMember(request, response);
+
+		try {
+			if (command.equals("addSelling")) {
+				addSelling(request, response);
 			}
-		}catch(Exception s){
+		} catch (Exception s) {
 			request.setAttribute("errorMsg", s.getMessage());
 			request.getRequestDispatcher("showError.jsp").forward(request, response);
 			s.printStackTrace();
 		}
 	}
 	
-	public void addMember(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void addSelling(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "showError.jsp";
 		
-		String email = request.getParameter("Email");
+		String email = request.getParameter("");
 		String password = request.getParameter("Password");
 		String phone = request.getParameter("Phone");
 		String address = request.getParameter("Address");
@@ -39,7 +38,7 @@ public class MemberController extends HttpServlet {
 		
 		MemberDTO member = new MemberDTO(email, password, phone, address, account);
 		try{
-			boolean result = ZigboService.addMember(member);
+			boolean result = ZigboService.addSelling(member);
 			if(result){
 				request.setAttribute("member", member);
 				request.setAttribute("successMsg", "가입 완료");
@@ -52,6 +51,5 @@ public class MemberController extends HttpServlet {
 		}
 		request.getRequestDispatcher(url).forward(request, response);
 	}
-
 
 }
